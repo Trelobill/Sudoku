816613fca36dc1b26b369e738eeb1491b276478a
Trelobill <vasilis_gagakis@yahoo.gr>
2022-01-29 15:55:13 +0200

    fwfw

---
+++ src/Sudoku.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.io.BufferedWriter;
+import java.io.*;
+import java.nio.file.*;
+
+public class Sudoku extends JFrame implements ActionListener, MouseListener {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+}
